014b1007945042fb3482211c6f442b8d60b8ac31
suraj255050 <117443202+suraj255050@users.noreply.github.com>
2023-01-01 02:44:55 +0530

    Add files via upload

---
+++ fassos database project questions ans .sql
@@ -0,0 +1,60 @@
+select * from customer_orders;
+select * from driver_order;
+select * from ingredients;
+select * from driver;
+select * from rolls;
+select * from rolls_recipes;
+
+Q.1 How many rolls were ordered?
+
+select count(roll_id) from customer_orders;
+
+Q.2 how many unique customer order were made?
+
+select count(distinct customer_id) from customer_orders;
+
+Q.3 how many successful order were delivered by each driver?
+
+select driver_id ,count(distinct order_id) from driver_order
+ where cancellation not in ('cancellation','cuatomer cancellation')
+ group by driver_id;
+ 
+ Q.4 how many of each type of roll was delivered?
+ 
+select roll_id,count(roll_id) 
+from customer_orders 
+where order_id in (
+select order_id from
+(select *,case when	cancellation in  ('cancellation','cuatomer cancellation') then 'c' else 'nc' end as order_cancel_details from driver_order)a 
+where order_cancel_details='nc') 
+group by roll_id;
+
+Q.5 how many veg and non veg rolls were ordered by each customer?
+
+select a.*,b.roll_name from
+(
+select customer_id,roll_id, count(roll_id)from customer_orders 
+group by customer_id,roll_id)a inner join rolls b on a.roll_id = b.roll_id;
+
+
+Q.6 what was the maximum number of rolls delivered in single order?
+ 
+select * from
+(
+select *,rank() over(order by cnt desc) rnk from
+(
+select order_id,count(roll_id) cnt
+from(
+select * from customer_orders
+where order_id in(select order_id from
+(select *,case when cancellation in  ('cancellation','cuatomer cancellation') then 'c' else 'nc' end as order_cancel_details from driver_order)a 
+where order_cancel_details='nc'))b
+group by order_id
+)C)d where rnk=1;
+
+
+Q.7 what is the number of order for each day of the week?
+
+select dow,count(distinct order_id) from
+(select *,dayname(order_date) dow from customer_orders)a
+group by dow;
