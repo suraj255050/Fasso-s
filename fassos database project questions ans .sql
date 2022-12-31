131da6e4629b5d09a2d1d7811e427a52179a34f5
suraj255050 <117443202+suraj255050@users.noreply.github.com>
2023-01-01 02:48:54 +0530

    Update fassos database project questions ans .sql

--- fassos database project questions ans .sql
+++ fassos database project questions ans .sql
@@ -1,3 +1,6 @@
+/* AUTHOR - SURAJ CHOWKI
+TOOL USED - MYSQL Workbench 8.0 CE
+
 select * from customer_orders;
 select * from driver_order;
 select * from ingredients;
@@ -6,21 +9,21 @@ select * from rolls;
 select * from rolls_recipes;
 
 Q.1 How many rolls were ordered?
-
+/* --------- SOLUTION --------- */
 select count(roll_id) from customer_orders;
 
 Q.2 how many unique customer order were made?
-
+/* --------- SOLUTION --------- */
 select count(distinct customer_id) from customer_orders;
 
 Q.3 how many successful order were delivered by each driver?
-
+/* --------- SOLUTION --------- */
 select driver_id ,count(distinct order_id) from driver_order
  where cancellation not in ('cancellation','cuatomer cancellation')
  group by driver_id;
  
  Q.4 how many of each type of roll was delivered?
- 
+ /* --------- SOLUTION --------- */
 select roll_id,count(roll_id) 
 from customer_orders 
 where order_id in (
@@ -30,7 +33,7 @@ where order_cancel_details='nc')
 group by roll_id;
 
 Q.5 how many veg and non veg rolls were ordered by each customer?
-
+/* --------- SOLUTION --------- */
 select a.*,b.roll_name from
 (
 select customer_id,roll_id, count(roll_id)from customer_orders 
@@ -38,7 +41,7 @@ group by customer_id,roll_id)a inner join rolls b on a.roll_id = b.roll_id;
 
 
 Q.6 what was the maximum number of rolls delivered in single order?
- 
+ /* --------- SOLUTION --------- */
 select * from
 (
 select *,rank() over(order by cnt desc) rnk from
@@ -54,7 +57,7 @@ group by order_id
 
 
 Q.7 what is the number of order for each day of the week?
-
+/* --------- SOLUTION --------- */
 select dow,count(distinct order_id) from
 (select *,dayname(order_date) dow from customer_orders)a
 group by dow;
